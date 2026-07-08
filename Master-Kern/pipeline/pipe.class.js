47ad9ed1864d15a0a62c207bf02edb76eae08fcd
iki1uc <entertrainer@web.de>
2026-07-08 23:50:00 +0200

    Create pipe.class.js

---
+++ Master-Kern/pipeline/pipe.class.js
@@ -0,0 +1,16 @@
+class PIPE {
+    constructor() {
+        this.respo = 0.42;
+        this.ghost = 0.27;
+        this.cache = 0.19;
+        this.tmp   = 0.12;
+    }
+
+    execute(input) {
+        let r = RESPO.exec(input);
+        let g = GHOST.correct(r);
+        let c = CACHE.preload(g);
+        let t = TMP.flush(c);
+        return t;
+    }
+}
